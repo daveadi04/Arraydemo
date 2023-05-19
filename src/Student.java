77625605d13ccdf1ac17d51aa1da6af844dd4135
Aditya dave <Aditya.dave@sheridancollege.ca>
2023-05-19 13:03:31 -0400

    first commit

---
+++ src/Student.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author adity
+ */
+public class studentlist {
+    public static void main(String[] args){
+        Student s1 = new Student("s1","Hevin");
+        Student[] studentList = new Student[3];
+        
+        studentList[0]=s1;
+        studentList[1]=new Student( "s2","abc");
+        studentList[2]=new Student( "s3","def");
+        for(int i=0;i<studentList.length;i++) {
+        System.out.println(studentList[i].getaName());
+    }
+       }
+            }
+    
+}
---
+++ src/studentList.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package arraysdemo;
+
+/**
+ *
+ * @author adity
+ */
+public class studentList {
+    public static void main(String[] args){
+        Student s1 = new Student("s1","Aditya");
+        Student[] studentList = new Student[3];
+        
+        studentList[0]=s1;
+        studentList[1]=new Student( "s2","abc");
+        studentList[2]=new Student( "s3","def");
+        for(int i=0;i<studentList.length;i++) {
+        System.out.println(studentList[i].getaName());
+    }
+       }
+            }
+    
+}
+
+    
+}
