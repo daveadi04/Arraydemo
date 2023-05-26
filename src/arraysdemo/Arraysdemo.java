e00f63da923f5a1e0be66c720ff8c48aa902c0e7
Aditya dave <Aditya.dave@sheridancollege.ca>
2023-05-26 12:33:47 -0400

    commit added

--- src/arraysdemo/Arraysdemo.java
+++ src/arraysdemo/Arraysdemo.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+//comment added 12.32
 package arraysdemo;
 
 import java.util.Scanner;
